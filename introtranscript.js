d79dc970e2ad29265fa7d2fc711dfd7a2f7f6b29
r-0n <ronywajah911@gmail.com>
2023-02-15 16:09:21 +0400

    update

--- introtranscript.js
+++ introtranscript.js
@@ -190,3 +190,36 @@ var swiper = new Swiper('.swiper-container', {
     },
 });
 // End of Section 3 Pricing Cards
+
+
+
+// Section 2 Video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+    if (video.paused) {
+        video.play()
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = '.7'
+    } else {
+        video.pause()
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+}
+
+btn.addEventListener('click', () => {
+    playPause()
+})
+
+video.addEventListener('timeupdate', () => {
+        const barWidth = video.currentTime / video.duration
+        bar.style.width = `${barWidth * 100}%`
+        if (video.ended) {
+            btn.className = 'far fa-play-circle'
+            video.style.opacity = '.3'
+        }
+    })
+    // End of Section 2 Video
